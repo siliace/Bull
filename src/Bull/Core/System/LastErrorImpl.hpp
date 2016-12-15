8150005c3ce172cb525591cbc92d1f5b7877b641
Siliace <benjber69@gmail.com>
2016-12-15 22:25:39 +0100

    Remove LastError class

--- src/Bull/Core/System/LastErrorImpl.hpp
+++
@@ -1,7 +0,0 @@
-#include <Bull/Core/System/Config.hpp>
-
-#if defined BULL_OS_WINDOWS
-    #include <Bull/Core/System/Win32/LastErrorImpl.hpp>
-#else
-    #include <Bull/Core/System/Unix/LastErrorImpl.hpp>
-#endif // defined
