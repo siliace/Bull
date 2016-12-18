745f876590abe6913d98fffa729f58b09716ac8d
Siliace <benjber69@gmail.com>
2016-12-18 23:41:01 +0100

    [Network/TcpSocket] We do not need to change the blocking mode to connect with timeout

--- src/Bull/Network/TcpSocket.cpp
+++ src/Bull/Network/TcpSocket.cpp
@@ -28,15 +28,9 @@ namespace Bull
         if(address.isValid() && port > 0)
         {
             Clock timer;
-            bool  blockingState = isEnableBlockingMode();
-            CallOnExit resetBlockingMode([this, blockingState]()
-            {
-                enableBlockingMode(blockingState);
-            });
 
             disconnect();
             create(address.getProtocol());
-            enableBlockingMode(false);
 
             timer.start();
 
