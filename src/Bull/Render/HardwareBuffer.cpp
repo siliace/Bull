9d4e45a5ddf2b2ebe3295ab1bf09903e5b42257d
Siliace <benjber69@gmail.com>
2016-12-24 11:05:38 +0100

    Cleanup

--- src/Bull/Render/HardwareBuffer.cpp
+++ src/Bull/Render/HardwareBuffer.cpp
@@ -5,33 +5,18 @@
 
 namespace Bull
 {
-    /*! \brief Bind a buffer
-     *
-     * \param buffer The buffer to bind
-     *
-     */
     void HardwareBuffer::bind(const HardwareBuffer& buffer)
     {
         ensureContext();
         gl::bindBuffer(buffer.m_type, buffer.m_id);
     }
 
-    /*! \brief Unbind any buffer
-     *
-     * \param type The type of buffer to unbind
-     *
-     */
     void HardwareBuffer::unbind(Type type)
     {
         ensureContext();
         gl::bindBuffer(type, 0);
     }
 
-    /*! \brief Constructor
-     *
-     * \param type The type the of OpenGL buffer to create
-     *
-     */
     HardwareBuffer::HardwareBuffer(Type type) :
         m_id(0),
         m_type(type)
@@ -39,22 +24,11 @@ namespace Bull
         /// Nothing
     }
 
-    /*! \brief Destructor
-     *
-     */
     HardwareBuffer::~HardwareBuffer()
     {
         destroy();
     }
 
-    /*! \brief Create the buffer in the VRAM
-     *
-     * \param size  The size of the buffer
-     * \param usage The usage of the buffer
-     *
-     * \return Return true if the buffer was created successfully, false otherwise
-     *
-     */
     bool HardwareBuffer::create(std::size_t size, Usage usage)
     {
         ensureContext();
@@ -71,16 +45,6 @@ namespace Bull
         return true;
     }
 
-    /*! \brief Fill the buffer
-     *
-     * \param data   Data to use to fill the buffer
-     * \param size   The size of data
-     * \param offset The offset of the data in the buffer
-     * \param discard True to flush the buffer before fill it, false to keep the original content
-     *
-     * \return Return true if the buffer was filled successfully, false otherwise
-     *
-     */
     bool HardwareBuffer::fill(const void* data, std::size_t size, std::size_t offset, bool discard)
     {
         ensureContext();
@@ -113,14 +77,8 @@ namespace Bull
         }
 
         return false;
-
     }
 
-    /*! \brief Flush the buffer
-     *
-     * \param keepMemory True to keep the allocated memory, false otherwise
-     *
-     */
     void HardwareBuffer::flush(bool keepMemory)
     {
         if(m_id)
@@ -139,9 +97,6 @@ namespace Bull
         }
     }
 
-    /*! \brief Destroy the buffer
-     *
-     */
     void HardwareBuffer::destroy()
     {
         if(m_id)
@@ -152,11 +107,6 @@ namespace Bull
         }
     }
 
-    /*! \brief Get the buffer system handler
-     *
-     * \return Return the native buffer system handler
-     *
-     */
     unsigned int HardwareBuffer::getSystemHandler() const
     {
         return m_id;
