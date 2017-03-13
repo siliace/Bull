820e6516e97ede8caf57e9460ab19207c1736246
Siliace <benjber69@gmail.com>
2017-03-13 21:09:05 +0900

    Add fonts files

---
+++ include/Bull/Render/Font.hpp
@@ -0,0 +1,32 @@
+#ifndef BULL_FONT_HPP
+#define BULL_FONT_HPP
+
+#include <Bull/Core/FileSystem/Path.hpp>
+
+namespace Bull
+{
+    class BULL_API Font
+    {
+    public:
+
+        /*! \brief Default constructor
+         *
+         */
+        Font();
+
+        /*! \brief Load a font from a file
+         *
+         * \param path The path of the font file
+         *
+         * \return True if the font was loaded successfully
+         *
+         */
+        bool loadFromPath(const Path& path);
+
+    private:
+
+
+    };
+}
+
+#endif // BULL_FONT_HPP
---
+++ include/Bull/Render/Glyph.hpp
@@ -0,0 +1,14 @@
+#ifndef BULL_GLYPH_HPP
+#define BULL_GLYPH_HPP
+
+#include <Bull/Core/System/Export.hpp>
+
+namespace Bull
+{
+    class BULL_API Glyph
+    {
+
+    };
+}
+
+#endif // BULL_GLYPH_hpP
