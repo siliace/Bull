6fea6d5a52e10941d2952fe0e9c8fbde02851517
Siliace <benjber69@gmail.com>
2017-03-13 21:27:43 +0900

    Utility/ImageLoader Add missing documentation

--- src/Bull/Utility/ImageLoader.hpp
+++ src/Bull/Utility/ImageLoader.hpp
@@ -1,7 +1,7 @@
 #ifndef Bull_ImageLoader_hpp
 #define Bull_ImageLoader_hpp
 
-#include <Bull/Core/String.hpp>
+#include <Bull/Core/FileSystem/Path.hpp>
 #include <Bull/Core/Integer.hpp>
 
 #include <Bull/Math/Vector/Vector2.hpp>
@@ -16,15 +16,63 @@ namespace Bull
         {
         public:
 
-            static bool load(const String& path, std::vector<Uint8>& pixels, Vector2UI& size);
+            /*! \brief Load an image
+             *
+             * \param path   The path of the image to load
+             * \param pixels Pixels of the loaded image
+             * \param size   The size of the loaded image
+             *
+             * \return True if the image was loaded successfully
+             *
+             */
+            static bool load(const Path& path, std::vector<Uint8>& pixels, Vector2UI& size);
 
-            static bool save(const String& path, Image::Format format, const std::vector<Uint8>& pixels, const Vector2UI& size);
+            /*! \brief Save an image
+             *
+             * \param path   The path of the file to create to save the image
+             * \param format The format to use to save the image
+             * \param pixels Pixels of the image to save
+             * \param size   The size of the image to save
+             *
+             * \return True if the image was saved successfully
+             *
+             */
+            static bool save(const Path& path, Image::Format format, const std::vector<Uint8>& pixels, const Vector2UI& size);
 
         private:
 
-            static bool saveBmp(const String& path, const std::vector<Uint8>& pixels, const Vector2UI& size);
-            static bool savePng(const String& path, const std::vector<Uint8>& pixels, const Vector2UI& size);
-            static bool saveTga(const String& path, const std::vector<Uint8>& pixels, const Vector2UI& size);
+            /*! \brief Save an image as .bmp file
+             *
+             * \param path   The path of the file to create to save the image
+             * \param pixels Pixels of the image to save
+             * \param size   The size of the image to save
+             *
+             * \return True if the image was saved successfully
+             *
+             */
+            static bool saveBmp(const Path& path, const std::vector<Uint8>& pixels, const Vector2UI& size);
+
+            /*! \brief Save an image as .png file
+             *
+             * \param path   The path of the file to create to save the image
+             * \param pixels Pixels of the image to save
+             * \param size   The size of the image to save
+             *
+             * \return True if the image was saved successfully
+             *
+             */
+            static bool savePng(const Path& path, const std::vector<Uint8>& pixels, const Vector2UI& size);
+
+            /*! \brief Save an image as .tga file
+             *
+             * \param path   The path of the file to create to save the image
+             * \param pixels Pixels of the image to save
+             * \param size   The size of the image to save
+             *
+             * \return True if the image was saved successfully
+             *
+             */
+            static bool saveTga(const Path& path, const std::vector<Uint8>& pixels, const Vector2UI& size);
         };
     }
 }
