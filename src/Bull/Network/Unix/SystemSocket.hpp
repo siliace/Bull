ad5cba4f2e1b664fcccc340d84fbe612e67de7ad
Siliace <benjber@gmail.com>
2017-05-29 21:33:06 +0900

    Remove shit

--- src/Bull/Network/Unix/SystemSocket.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef BULL_SYSTEMSOCKET_HPP
-#define BULL_SYSTEMSOCKET_HPP
-
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#endif // BULL_SYSTEMSOCKET_HPP
--- src/Bull/Network/Win32/SystemSocket.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BULL_SYSTEMSOCKET_HPP
-#define BULL_SYSTEMSOCKET_HPP
-
-#include <Bull/Core/System/OS.hpp>
-
-#include <winsock2.h>
-#include <ws2tcpip.h>
-
-#endif // BULL_SYSTEMSOCKET_HPP
