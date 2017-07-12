f3f6fe0960d407a254b63914e9c2414577053778
Siliace <benjber69@gmail.com>
2017-07-12 22:51:07 +0200

    [Core/Signal] Refactor

--- include/Bull/Core/Pattern/Signal.hpp
+++ include/Bull/Core/Pattern/Signal.hpp
@@ -1,134 +1,202 @@
-#ifndef BULL_SIGNAL_HPP
-#define BULL_SIGNAL_HPP
+#ifndef BULL_CORE_PATTERN_SIGNAL_HPP
+#define BULL_CORE_PATTERN_SIGNAL_HPP
 
 #include <functional>
-#include <memory>
 #include <vector>
 
-#include <Bull/Core/Export.hpp>
+#include <Bull/Core/Pattern/NonCopyable.hpp>
+#include <Bull/Core/System/Integer.hpp>
+#include <Bull/Core/Thread/Lock.hpp>
 
 namespace Bull
 {
     template <typename... Args>
-    class BULL_CORE_API Signal
+    class Signal : public NonCopyable
     {
     public:
-
-        using Callback = std::function<void(Args...)>;
 
         class Slot
         {
         public:
 
-            /*! \brief Constructor
-             *
-             * \param source The source signal
-             *
-             */
-            Slot(const Signal* source, const Callback& callback) :
-                m_source(source),
-                m_callback(callback)
-            {
-                /// Nothing
-            }
-
-            /*! \brief Get the source signal of the Slot
-             *
-             * \return The source signal
-             *
-             */
-            const Signal* getSource() const
-            {
-                return m_source;
-            }
-
-            /*! \brief Call the Slot
-             *
-             * \param
-             *
-             */
-            void trigger(Args... args) const
-            {
-                m_callback(args...);
-            }
+            using Function = std::function<void(Args...)>;
 
         private:
 
-            const Signal* m_source;
-            Callback      m_callback;
+            static Uint64 s_nextId;
+
+            static Mutex s_idMutex;
+
+        public:
+
+            /*! \brief Default constructor
+             *
+             */
+            Slot();
+
+            /*! \brief Constructor
+             *
+             * \param function The function handled by the Slot
+             *
+             */
+            Slot(const Function& function);
+
+            /*! \brief Constructor
+             *
+             * \param instance
+             * \param method
+             *
+             */
+            template <typename Object>
+            Slot(Object* instance, void(Object::*method)(Args...));
+
+            /*! \brief Constructor
+             *
+             * \param instance
+             * \param method
+             *
+             */
+            template <typename Object>
+            Slot(Object& instance, void(Object::*method)(Args...));
+
+            /*! \brief Constructor
+             *
+             * \param instance
+             * \param method
+             *
+             */
+            template <typename Object>
+            Slot(const Object* instance, void(Object::*method)(Args...) const);
+
+            /*! \brief Constructor
+             *
+             * \param instance
+             * \param method
+             *
+             */
+            template <typename Object>
+            Slot(const Object& instance, void(Object::*method)(Args...) const);
+
+            /*! \brief Destructor
+             *
+             */
+            ~Slot();
+
+            /*! \brief Reset the function handled by the Slot
+             *
+             * \param function The function handled by the Slot
+             *
+             */
+            void reset(const Function& function);
+
+            /*! \brief
+             *
+             * \param instance
+             * \param method
+             *
+             */
+            template <typename Object>
+            void reset(Object* instance, void(Object::*method)(Args...));
+
+            /*! \brief
+             *
+             * \param instance
+             * \param method
+             *
+             */
+            template <typename Object>
+            void reset(Object& instance, void(Object::*method)(Args...));
+
+            /*! \brief
+             *
+             * \param instance
+             * \param method
+             *
+             */
+            template <typename Object>
+            void reset(const Object* instance, void(Object::*method)(Args...) const);
+
+            /*! \brief
+             *
+             * \param instance
+             * \param method
+             *
+             */
+            template <typename Object>
+            void reset(const Object& instance, void(Object::*method)(Args...) const);
+
+        private:
+
+            friend class Signal<Args...>;
+
+            /*! \brief Call the function handled by the Slot
+             *
+             * \param Arguments to sent to the function
+             *
+             */
+            void call(Args... args) const;
+
+            /*! \brief Get the ID of the Slot
+             *
+             * \return The ID
+             *
+             */
+            Uint64 getId() const;
+
+            /*! \brief Add a Signal to the listening Signals
+             *
+             * \param signal The Signal to add
+             *
+             */
+            void addSignal(Signal<Args...>* signal);
+
+        private:
+
+            Uint64                        m_id;
+            std::vector<Signal<Args...>*> m_signals;
+            Function                      m_function;
         };
-
-    private:
-
-        using SlotPtr  = std::shared_ptr<Slot>;
-        using SlotList = std::vector<SlotPtr>;
 
     public:
 
-        /*! \brief Default constructor
+        /*! \brief Bind a Slot to this Signal
+         *
+         * \param slot The Slot to bind
          *
          */
-        Signal() = default;
+        void bind(Slot& slot);
 
-        /*! \brief Connect a function to the Signal
+        /*! \brief Emit a Signal
          *
-         * \param callback The function
-         *
-         */
-        void connect(const Callback& callback)
-        {
-            m_slots.push_back(std::make_shared<Slot>(this, callback));
-        }
-
-        template <typename O>
-        void connect(O& instance, void(O::*method)(Args...))
-        {
-            connect([instance, method](Args... args){
-                (instance.*method)(std::forward<Args>(args)...);
-            });
-        }
-
-        template <typename O>
-        void connect(const O& instance, void(O::*method)(Args...) const)
-        {
-            connect([instance, method](Args... args){
-                (instance.*method)(std::forward<Args>(args)...);
-            });
-        }
-
-        template <typename O>
-        void connect(O* instance, void(O::*method)(Args...))
-        {
-            connect([instance, method](Args... args){
-                (instance->*method)(std::forward<Args>(args)...);
-            });
-        }
-
-        template <typename O>
-        void connect(const O* instance, void(O::*method)(Args...) const)
-        {
-            connect([instance, method](Args... args){
-                (instance->*method)(std::forward<Args>(args)...);
-            });
-        }
-
-        /*! \brief Emit the Signal
-         *
-         * \param args Arguments to send to slots
+         * \param args Arguments to send to listening slots
          *
          */
-        void operator()(Args... args)
-        {
-            for(const SlotPtr& slot : m_slots)
-            {
-                slot->trigger(args...);
-            }
-        }
+        void operator()(Args... args) const;
 
     private:
 
-        SlotList m_slots;
+        friend class Slot;
+
+        /*! \brief Unbind a slot from this Signal
+         *
+         * \param slotId The Id of the Slot to unbind
+         *
+         */
+        void unbind(Uint64 slotId);
+
+    private:
+
+        std::vector<Slot> m_slots;
     };
+
+    template <typename... Args>
+    Uint64 Signal<Args...>::Slot::s_nextId = 0;
+
+    template <typename... Args>
+    Mutex Signal<Args...>::Slot::s_idMutex;
 }
+
+#include <Bull/Core/Pattern/Signal.inl>
+#include <Bull/Core/Pattern/Slot.inl>
 
 #endif // BULL_SIGNAL_HPP
