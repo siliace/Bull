5c182d4c8001931e6cb852c29cbeb94d4651481b
Siliace <benjber69@gmail.com>
2017-09-20 01:08:35 +0200

    [Core/Windows] Create a file to include windows headers

---
+++ src/Bull/Core/Support/Win32/Windows.hpp
@@ -0,0 +1,27 @@
+#ifndef BULL_CORE_SUPPORT_WINDOWS_HPP
+#define BULL_CORE_SUPPORT_WINDOWS_HPP
+
+#include <Bull/Core/Configuration/OS.hpp>
+
+#ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN 1
+#endif
+
+#ifndef NOMINMAX
+    #define NOMINMAX
+#endif
+
+#if defined _WIN32_WINNT
+    #if _WIN32_WINNT < BULL_WINDOWS_VERSION
+        #undef _WIN32_WINNT
+        #define _WIN32_WINNT BULL_WINDOWS_VERSION
+    #endif
+#else
+    #define _WIN32_WINNT BULL_WINDOWS_VERSION
+#endif
+
+#include <windows.h>
+#include <windowsx.h>
+#include <mmsystem.h>
+
+#endif // BULL_CORE_SUPPORT_WINDOWS_HPP
