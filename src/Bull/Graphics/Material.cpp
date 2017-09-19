99299e46fc6fc82fbb696295efbca0f392b9dc55
Siliace <benjber69@gmail.com>
2017-09-19 22:38:03 +0200

    [Graphics/Material] Draft implementation

---
+++ src/Bull/Graphics/Material.cpp
@@ -0,0 +1,34 @@
+#include <Bull/Graphics/Material.hpp>
+
+namespace Bull
+{
+    Material::Material() :
+        m_shininess(0.f)
+    {
+        /// Nothing
+    }
+
+    Material& Material::setShininess(float shininess)
+    {
+        m_shininess = shininess;
+
+        return (*this);
+    }
+
+    float Material::getShininess() const
+    {
+        return m_shininess;
+    }
+
+    Material& Material::setTexture(TextureRef texture, TextureType::TextureType type)
+    {
+        m_textures[type] = std::move(texture);
+
+        return (*this);
+    }
+
+    const TextureRef& Material::getTexture(TextureType::TextureType type) const
+    {
+        return m_textures[type];
+    }
+}
