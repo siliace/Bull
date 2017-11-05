e05e25717cc3999659c5dcb28201489006391970
Siliace <benjber69@gmail.com>
2017-11-05 15:26:36 +0100

    [Network/IpAddressV6] Implement toString method

--- src/Bull/Network/Address/IpAddressV6.cpp
+++ src/Bull/Network/Address/IpAddressV6.cpp
@@ -1,3 +1,5 @@
+#include <Bull/Core/IO/StringStream.hpp>
+
 #include <Bull/Network/Address/IpAddressV6.hpp>
 
 namespace Bull
@@ -15,7 +17,19 @@ namespace Bull
 
     String IpAddressV6::toString() const
     {
-        return "";
+        StringStream ss;
+
+        for(std::size_t i = 0; i < getByteCount(); i++)
+        {
+            ss << m_bytes[i];
+
+            if(i < getByteCount() - 1)
+            {
+                ss << ':';
+            }
+        }
+
+        return ss.toString();
     }
 
     NetProtocol IpAddressV6::getProtocol() const
@@ -25,19 +39,19 @@ namespace Bull
 
     std::size_t IpAddressV6::getByteCount() const
     {
-        return 16;
+        return m_bytes.size();
     }
 
     Uint8& IpAddressV6::at(std::size_t index)
     {
-        RangeCheck(index, 16);
+        RangeCheck(index, getByteCount());
 
         return m_bytes[index];
     }
 
     Uint8 IpAddressV6::at(std::size_t index) const
     {
-        RangeCheck(index, 16);
+        RangeCheck(index, getByteCount());
 
         return m_bytes[index];
     }
