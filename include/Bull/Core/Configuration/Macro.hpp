da8a15b76e463f69b710e18906348a7a8a69e623
Siliace <benjber69@gmail.com>
2017-11-11 17:41:52 +0100

    [Core/Macro] Add unused macro

--- include/Bull/Core/Configuration/Macro.hpp
+++ include/Bull/Core/Configuration/Macro.hpp
@@ -17,4 +17,8 @@
     #define BULL_ZERO_MEMORY_PTR(Pointer, Length) std::memset(Pointer, 0, Length)
 #endif
 
+#ifndef BULL_UNUSED
+    #define BULL_UNUSED(Variable) (void)Variable
+#endif
+
 #endif // BULL_CORE_CONFIGURATION_MACRO_HPP
