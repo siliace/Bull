32a6ebdce3ed4d829c3a42da69af80d1ff98837f
Siliace <benjber69@gmail.com>
2018-01-07 13:58:12 +0100

    [Core/Integer] Remove useless include

--- include/Bull/Core/Configuration/Integer.hpp
+++ include/Bull/Core/Configuration/Integer.hpp
@@ -2,19 +2,17 @@
 #define BULL_CORE_CONFIGURATION_INTEGER_HPP
 
 #include <cstdint>
-#include <utility>
 
 namespace Bull
 {
+    typedef int8_t   Int8;
+    typedef int16_t  Int16;
+    typedef int32_t  Int32;
+    typedef int64_t  Int64;
     typedef uint8_t  Uint8;
     typedef uint16_t Uint16;
     typedef uint32_t Uint32;
     typedef uint64_t Uint64;
-
-    typedef int8_t  Int8;
-    typedef int16_t Int16;
-    typedef int32_t Int32;
-    typedef int64_t Int64;
 }
 
 #endif // BULL_CORE_CONFIGURATION_INTEGER_HPP
