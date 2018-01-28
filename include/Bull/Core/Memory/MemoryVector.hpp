4ae12e2f98df94418587832f1b0528cf4663ee96
Siliace <benjber69@gmail.com>
2018-01-28 12:54:57 +0100

    [Core/MemoryVector] Remove .inl file

--- include/Bull/Core/Memory/MemoryVector.hpp
+++ include/Bull/Core/Memory/MemoryVector.hpp
@@ -1,9 +1,11 @@
 #ifndef BULL_CORE_MEMORY_ARRAY_HPP
 #define BULL_CORE_MEMORY_ARRAY_HPP
 
+#include <cstring>
 #include <vector>
 
 #include <Bull/Core/Memory/AbstractBuffer.hpp>
+#include <Bull/Core/Memory/RangeCheck.hpp>
 
 namespace Bull
 {
@@ -22,7 +24,11 @@ namespace Bull
          * \param size The size of the Array
          *
          */
-        explicit MemoryVector(std::size_t size);
+        explicit MemoryVector(std::size_t size) :
+            MemoryVector<T>(size, T())
+        {
+            /// nothing
+        }
 
         /*! \brief Constructor
          *
@@ -30,7 +36,11 @@ namespace Bull
          * \param value The value to the Array
          *
          */
-        MemoryVector(std::size_t size, T value);
+        MemoryVector(std::size_t size, T value) :
+            m_array(size, value)
+        {
+            /// nothing
+        }
 
         /*! \brief Create the Array
          *
@@ -39,7 +49,17 @@ namespace Bull
          * \return True if the Array was created successfully
          *
          */
-        bool create(std::size_t capacity) override;
+        bool create(std::size_t capacity) override
+        {
+            if(isEmpty())
+            {
+                m_array.resize(capacity);
+
+                return true;
+            }
+
+            return false;
+        }
 
         /*! \brief Fill the buffer
          *
@@ -50,7 +70,22 @@ namespace Bull
          * \return True if the buffer was filled successfully
          *
          */
-        bool fill(const void* data, std::size_t size, std::size_t offset = 0) override;
+        bool fill(const void* data, std::size_t size, std::size_t offset = 0) override
+        {
+            if(data && size)
+            {
+                if(getCapacity() < size + offset)
+                {
+                    resize(size + offset);
+                }
+
+                std::memcpy(&m_array[offset], data, size);
+
+                return true;
+            }
+
+            return false;
+        }
 
         /*! \brief Access operator
          *
@@ -59,7 +94,12 @@ namespace Bull
          * \return The value
          *
          */
-        T& at(std::size_t index);
+        T& at(std::size_t index)
+        {
+            RangeCheck(index, getCapacity());
+
+            return m_array[index];
+        }
 
         /*! \brief Access operator
          *
@@ -68,40 +108,60 @@ namespace Bull
          * \return The value
          *
          */
-        const T& at(std::size_t index) const;
+        const T& at(std::size_t index) const
+        {
+            RangeCheck(index, getCapacity());
+
+            return m_array[index];
+        }
 
         /*! \brief Resize the Array
          *
          * \param size The size
          *
          */
-        void resize(std::size_t size);
+        void resize(std::size_t size)
+        {
+            m_array.resize(size);
+        }
 
         /*! \brief Clear the Array
          *
          */
-        void clear() override;
+        void clear() override
+        {
+            m_array.clear();
+        }
 
         /*! \brief Tell whether the Array is empty
          *
          * \return True if the Array is empty
          *
          */
-        bool isEmpty() const;
+        bool isEmpty() const
+        {
+            return m_array.empty();
+        }
 
         /*! \brief Get a pointer to the Array
          *
          * \return The pointer
          *
          */
-        const T* getBuffer() const;
+        const T* getBuffer() const
+        {
+            return m_array.data();
+        }
 
         /*! \brief Get the capacity of the Array
          *
          * \return The capacity
          *
          */
-        std::size_t getCapacity() const override;
+        std::size_t getCapacity() const override
+        {
+            return m_array.size();
+        }
 
         /*! \brief Access operator
          *
@@ -110,7 +170,10 @@ namespace Bull
          * \return The value
          *
          */
-        T& operator[](std::size_t index);
+        T& operator[](std::size_t index)
+        {
+            return at(index);
+        }
 
         /*! \brief Access operator
          *
@@ -119,14 +182,15 @@ namespace Bull
          * \return The value
          *
          */
-        const T& operator[](std::size_t index) const;
+        const T& operator[](std::size_t index) const
+        {
+            return at(index);
+        }
 
     private:
 
         std::vector<T> m_array;
     };
 }
-
-#include <Bull/Core/Memory/MemoryVector.inl>
 
 #endif // BULL_CORE_MEMORY_ARRAY_HPP
