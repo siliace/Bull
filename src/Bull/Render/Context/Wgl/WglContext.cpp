794568dee9387bde7f3e2ed70454c523b47133fe
Siliace <benjber69@gmail.com>
2018-03-21 19:14:38 +0100

    [Render/WglContext] Throw an InternalError when trying to make a context with a PBuffer than became invalid

--- src/Bull/Render/Context/Wgl/WglContext.cpp
+++ src/Bull/Render/Context/Wgl/WglContext.cpp
@@ -1,6 +1,7 @@
 #include <limits>
 
-#include <Bull/Core/Log/Log.hpp>
+#include <Bull/Core/Exception/Throw.hpp>
+#include <Bull/Core/Exception/InternalError.hpp>
 #include <Bull/Core/System/Library.hpp>
 
 #include <Bull/Render/Context/Wgl/WglContext.hpp>
@@ -52,13 +53,13 @@ namespace Bull
             if(wglPixelFormat.isLoaded())
             {
                 static const int attribs[] =
-                {
-                    WGL_DRAW_TO_WINDOW_ARB, 1,
-                    WGL_SUPPORT_OPENGL_ARB, 1,
-                    WGL_DOUBLE_BUFFER_ARB,  1,
-                    WGL_PIXEL_TYPE_ARB,     WGL_TYPE_RGBA_ARB,
-                    0
-                };
+                        {
+                                WGL_DRAW_TO_WINDOW_ARB, 1,
+                                WGL_SUPPORT_OPENGL_ARB, 1,
+                                WGL_DOUBLE_BUFFER_ARB, 1,
+                                WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+                                0
+                        };
 
                 int formats[512];
                 UINT count = 0;
@@ -73,15 +74,15 @@ namespace Bull
                         int format[7] = {0};
                         int sample[2] = {0};
                         static const int formatAttribs[] =
-                        {
-                            WGL_RED_BITS_ARB,
-                            WGL_GREEN_BITS_ARB,
-                            WGL_BLUE_BITS_ARB,
-                            WGL_ALPHA_BITS_ARB,
-                            WGL_DEPTH_BITS_ARB,
-                            WGL_STENCIL_BITS_ARB,
-                            WGL_ACCELERATION_ARB
-                        };
+                                {
+                                        WGL_RED_BITS_ARB,
+                                        WGL_GREEN_BITS_ARB,
+                                        WGL_BLUE_BITS_ARB,
+                                        WGL_ALPHA_BITS_ARB,
+                                        WGL_DEPTH_BITS_ARB,
+                                        WGL_STENCIL_BITS_ARB,
+                                        WGL_ACCELERATION_ARB
+                                };
 
                         if(!wglGetPixelFormatAttribiv(device, formats[i], PFD_MAIN_PLANE, 7, formatAttribs, format))
                         {
@@ -91,10 +92,10 @@ namespace Bull
                         if(isSupported("WGL_ARB_multisample"))
                         {
                             static const int sampleAttribs[] =
-                            {
-                                WGL_SAMPLE_BUFFERS_ARB,
-                                WGL_SAMPLES_ARB
-                            };
+                                    {
+                                            WGL_SAMPLE_BUFFERS_ARB,
+                                            WGL_SAMPLES_ARB
+                                    };
 
                             if(!wglGetPixelFormatAttribiv(device, formats[i], PFD_MAIN_PLANE, 2, sampleAttribs, sample))
                             {
@@ -106,15 +107,14 @@ namespace Bull
                         {
                             int pbuffer;
                             static const int pbufferAttribs[] =
-                            {
-                                WGL_DRAW_TO_PBUFFER_ARB
-                            };
+                                    {
+                                            WGL_DRAW_TO_PBUFFER_ARB
+                                    };
 
                             if(!wglGetPixelFormatAttribiv(device, formats[i], PFD_MAIN_PLANE, 1, pbufferAttribs, &pbuffer))
                             {
                                 break;
-                            }
-                            else if(pbuffer != 1)
+                            }else if(pbuffer != 1)
                             {
                                 continue;
                             }
@@ -124,11 +124,11 @@ namespace Bull
                         if(format[6])
                         {
                             int colors = format[0] + format[1] + format[2] + format[3];
-                            int score  = evaluatePixelFormat(colors, format[4], format[5], sample[0] ? sample[1] : 0, bitsPerPixel, settings);
+                            int score = evaluatePixelFormat(colors, format[4], format[5], sample[0] ? sample[1] : 0, bitsPerPixel, settings);
 
                             if(bestScore > score)
                             {
-                                bestScore       = score;
+                                bestScore = score;
                                 bestPixelFormat = formats[i];
                             }
                         }
@@ -145,15 +145,15 @@ namespace Bull
             {
                 PIXELFORMATDESCRIPTOR descriptor;
                 ZeroMemory(&descriptor, sizeof(descriptor));
-                descriptor.nSize        = sizeof(descriptor);
-                descriptor.nVersion     = 1;
-                descriptor.iLayerType   = PFD_MAIN_PLANE;
-                descriptor.dwFlags      = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
-                descriptor.iPixelType   = PFD_TYPE_RGBA;
-                descriptor.cColorBits   = bitsPerPixel;
-                descriptor.cDepthBits   = settings.depths;
+                descriptor.nSize = sizeof(descriptor);
+                descriptor.nVersion = 1;
+                descriptor.iLayerType = PFD_MAIN_PLANE;
+                descriptor.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+                descriptor.iPixelType = PFD_TYPE_RGBA;
+                descriptor.cColorBits = bitsPerPixel;
+                descriptor.cDepthBits = settings.depths;
                 descriptor.cStencilBits = settings.stencil;
-                descriptor.cAlphaBits   = bitsPerPixel == 32 ? 8 : 0;
+                descriptor.cAlphaBits = bitsPerPixel == 32 ? 8 : 0;
 
                 bestPixelFormat = ChoosePixelFormat(device, &descriptor);
             }
@@ -162,17 +162,17 @@ namespace Bull
         }
 
         WglContext::WglContext(const std::shared_ptr<WglContext>& shared) :
-            WglContext(shared, VideoMode(1, 1), ContextSettings())
+                WglContext(shared, VideoMode(1, 1), ContextSettings())
         {
             /// Nothing
         }
 
         WglContext::WglContext(const std::shared_ptr<WglContext>& shared, const VideoMode& mode, const ContextSettings& settings) :
-            GlContext(settings),
-            m_device(nullptr),
-            m_render(nullptr),
-            m_pbuffer(nullptr),
-            m_ownWindow(false)
+                GlContext(settings),
+                m_device(nullptr),
+                m_render(nullptr),
+                m_pbuffer(nullptr),
+                m_ownWindow(false)
         {
             createSurface(shared, mode.width, mode.height, mode.bitsPerPixel);
 
@@ -185,17 +185,17 @@ namespace Bull
         }
 
         WglContext::WglContext(const std::shared_ptr<WglContext>& shared, Uint8 bitsPerPixel, const ContextSettings& settings) :
-            WglContext(shared, VideoMode(1, 1, bitsPerPixel), settings)
+                WglContext(shared, VideoMode(1, 1, bitsPerPixel), settings)
         {
             /// Nothing
         }
 
         WglContext::WglContext(const std::shared_ptr<WglContext>& shared, const std::unique_ptr<WindowImpl>& window, Uint8 bitsPerPixel, const ContextSettings& settings) :
-            GlContext(settings),
-            m_device(nullptr),
-            m_render(nullptr),
-            m_pbuffer(nullptr),
-            m_ownWindow(false)
+                GlContext(settings),
+                m_device(nullptr),
+                m_render(nullptr),
+                m_pbuffer(nullptr),
+                m_ownWindow(false)
         {
             createSurface(window);
 
@@ -225,8 +225,7 @@ namespace Bull
                 {
                     wglReleasePbufferDC(m_pbuffer, m_device);
                     wglDestroyPbuffer(m_pbuffer);
-                }
-                else
+                }else
                 {
                     ReleaseDC(m_window, m_device);
                 }
@@ -261,6 +260,17 @@ namespace Bull
 
         bool WglContext::makeCurrent()
         {
+            if(m_pbuffer)
+            {
+                int flag = 0;
+                wglQueryPbuffer(m_pbuffer, WGL_PBUFFER_LOST_ARB, &flag);
+
+                if(flag)
+                {
+                    Throw(InternalError, "WglContext::makeCurrent", "PBuffer is became invalid");
+                }
+            }
+
             return wglMakeCurrent(m_device, m_render) == TRUE;
         }
 
@@ -296,13 +306,13 @@ namespace Bull
             if(!m_device)
             {
                 m_window = CreateWindow("STATIC", nullptr,
-                                         WS_DISABLED | WS_POPUP,
-                                         0, 0,
-                                         width, height,
-                                         nullptr,
-                                         nullptr,
-                                         GetModuleHandle(nullptr),
-                                         nullptr);
+                                        WS_DISABLED | WS_POPUP,
+                                        0, 0,
+                                        width, height,
+                                        nullptr,
+                                        nullptr,
+                                        GetModuleHandle(nullptr),
+                                        nullptr);
 
                 m_device = GetDC(m_window);
 
@@ -315,7 +325,7 @@ namespace Bull
             PIXELFORMATDESCRIPTOR descriptor;
             int bestFormat = getBestPixelFormat(m_device, bitsPerPixel, m_settings);
 
-            descriptor.nSize    = sizeof(PIXELFORMATDESCRIPTOR);
+            descriptor.nSize = sizeof(PIXELFORMATDESCRIPTOR);
             descriptor.nVersion = 1;
             DescribePixelFormat(m_device, bestFormat, sizeof(PIXELFORMATDESCRIPTOR), &descriptor);
 
@@ -339,7 +349,7 @@ namespace Bull
 
                     if(isSupported("WGL_ARB_create_context_profile"))
                     {
-                        int flags   = 0;
+                        int flags = 0;
                         int profile = (m_settings.profile == ContextSettingsProfile_Compatibility) ? WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB : WGL_CONTEXT_CORE_PROFILE_BIT_ARB;
 
                         if(m_settings.type == ContextSettingsType_Debug)
@@ -364,9 +374,9 @@ namespace Bull
                         {
                             attribs.push_back(WGL_CONTEXT_OPENGL_NO_ERROR_ARB);
                             attribs.push_back(1);
-                        }
-                        else
-                        {   m_settings.type = ContextSettingsType_Default;
+                        }else
+                        {
+                            m_settings.type = ContextSettingsType_Default;
                             m_log->warning("WGL_CONTEXT_OPENGL_NO_ERROR_ARB is not available");
                         }
                     }
@@ -383,14 +393,12 @@ namespace Bull
                         if(m_settings.minor == 0)
                         {
                             m_settings.major -= 1;
-                            m_settings.minor  = 9;
-                        }
-                        else
+                            m_settings.minor = 9;
+                        }else
                         {
                             m_settings.minor -= 1;
                         }
-                    }
-                    else
+                    }else
                     {
                         m_log->info("Create WglContext with version " + String::number(m_settings.major) + "." + String::number(m_settings.minor));
                     }
@@ -419,7 +427,7 @@ namespace Bull
         {
             PIXELFORMATDESCRIPTOR pfd;
             pfd.nVersion = 1;
-            pfd.nSize    = sizeof(PIXELFORMATDESCRIPTOR);
+            pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
 
             int pixelFormat = GetPixelFormat(m_device);
             DescribePixelFormat(m_device, pixelFormat, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
@@ -429,19 +437,18 @@ namespace Bull
                 int format[2] = {0};
 
                 static const int formatAttribs[] =
-                {
-                    WGL_DEPTH_BITS_ARB,
-                    WGL_STENCIL_BITS_ARB
-                };
+                        {
+                                WGL_DEPTH_BITS_ARB,
+                                WGL_STENCIL_BITS_ARB
+                        };
 
                 if(wglGetPixelFormatAttribiv(m_device, pixelFormat, PFD_MAIN_PLANE, 2, formatAttribs, format))
                 {
-                    m_settings.depths  = static_cast<Uint8>(format[0]);
+                    m_settings.depths = static_cast<Uint8>(format[0]);
                     m_settings.stencil = static_cast<Uint8>(format[1]);
-                }
-                else
+                }else
                 {
-                    m_settings.depths  = pfd.cDepthBits;
+                    m_settings.depths = pfd.cDepthBits;
                     m_settings.stencil = pfd.cStencilBits;
                 }
 
@@ -449,29 +456,26 @@ namespace Bull
                 {
                     int sample[2] = {0};
                     static const int sampleAttribs[] =
-                    {
-                        WGL_SAMPLE_BUFFERS_ARB,
-                        WGL_SAMPLES_ARB
-                    };
+                            {
+                                    WGL_SAMPLE_BUFFERS_ARB,
+                                    WGL_SAMPLES_ARB
+                            };
 
                     if(wglGetPixelFormatAttribiv(m_device, pixelFormat, PFD_MAIN_PLANE, 2, sampleAttribs, sample))
                     {
                         m_settings.antialiasing = sample[0] ? sample[1] : 0;
-                    }
-                    else
+                    }else
                     {
                         m_settings.antialiasing = 0;
                     }
-                }
-                else
+                }else
                 {
                     m_settings.antialiasing = 0;
                 }
-            }
-            else
+            }else
             {
-                m_settings.depths       = pfd.cDepthBits;
-                m_settings.stencil      = pfd.cStencilBits;
+                m_settings.depths = pfd.cDepthBits;
+                m_settings.stencil = pfd.cStencilBits;
                 m_settings.antialiasing = 0;
             }
         }
