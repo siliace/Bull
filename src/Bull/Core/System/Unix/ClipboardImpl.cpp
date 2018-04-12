ea268bbd8a86317c56538427216785f7d6abcda9
Benjamin Bernard <bbernard@e-biz.fr>
2018-04-12 13:04:19 +0200

    [Core/ClipboardImplUnix] Throw UnsupportedOperation on every method of the clipboard on Unix systems

--- src/Bull/Core/System/Unix/ClipboardImpl.cpp
+++ src/Bull/Core/System/Unix/ClipboardImpl.cpp
@@ -1,3 +1,5 @@
+#include <Bull/Core/Exception/Throw.hpp>
+#include <Bull/Core/Exception/UnsupportedOperation.hpp>
 #include <Bull/Core/System/Win32/ClipboardImpl.hpp>
 
 namespace Bull
@@ -6,17 +8,17 @@ namespace Bull
     {
         void ClipboardImpl::flush()
         {
-
+            Throw(UnsupportedOperation, "ClipboardImpl::flush", "Operation not implemented yet");
         }
 
         void ClipboardImpl::setContent(const String& content)
         {
-
+            Throw(UnsupportedOperation, "ClipboardImpl::setContent", "Operation not implemented yet");
         }
 
         String ClipboardImpl::getContent()
         {
-            return "";
+            Throw(UnsupportedOperation, "ClipboardImpl::getContent", "Operation not implemented yet");
         }
     }
 }
