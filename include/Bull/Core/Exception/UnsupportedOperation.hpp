5281c177aec9eed2f8bbaf46c78b517d6100c794
Benjamin Bernard <bbernard@e-biz.fr>
2018-04-12 13:03:26 +0200

    [Core/UnsupportedOperation] Create an unsupported operation exception

---
+++ include/Bull/Core/Exception/UnsupportedOperation.hpp
@@ -0,0 +1,11 @@
+#ifndef BULL_CORE_EXCEPTION_UNSUPPORTEDOPERATION_HPP
+#define BULL_CORE_EXCEPTION_UNSUPPORTEDOPERATION_HPP
+
+#include <Bull/Core/Exception/DeclareException.hpp>
+
+namespace Bull
+{
+    DeclareException(UnsupportedOperation);
+}
+
+#endif // BULL_CORE_EXCEPTION_UNSUPPORTEDOPERATION_HPP
