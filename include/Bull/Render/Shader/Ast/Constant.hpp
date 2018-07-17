d2a78b96088c049e8db43fb07a414aa3b96a9af1
Benjamin Bernard <bbernard@e-biz.fr>
2018-07-17 13:18:10 +0200

    [Render/Ast] Add constant AST variable type

---
+++ include/Bull/Render/Shader/Ast/Constant.hpp
@@ -0,0 +1,34 @@
+#ifndef BULL_RENDER_SHADER_AST_CONSTANT_HPP
+#define BULL_RENDER_SHADER_AST_CONSTANT_HPP
+
+#include <Bull/Render/Shader/Ast/Variable.hpp>
+
+namespace Bull
+{
+    namespace Ast
+    {
+        template <typename T>
+        class BULL_RENDER_API Constant : public Variable<T>
+        {
+        public:
+
+            Constant(const String& name, const T& value) :
+                    Variable<T>(name),
+                    m_value(value)
+            {
+                /// Nothing
+            }
+
+            const T& getValue() const
+            {
+                return m_value;
+            }
+
+        private:
+
+            T m_value;
+        };
+    }
+}
+
+#endif // BULL_RENDER_SHADER_AST_CONSTANT_HPP
