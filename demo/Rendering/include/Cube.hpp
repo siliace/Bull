b29ba9c1b9783558fb5bc623b96673ae16241689
Siliace <benjber69@gmail.com>
2018-07-25 23:58:57 +0200

    [Demo/Rendering] Fix build

--- demo/Rendering/include/Cube.hpp
+++ demo/Rendering/include/Cube.hpp
@@ -1,9 +1,8 @@
 #ifndef CUBE_HPP
 #define CUBE_HPP
 
-#include <Bull/Graphics/Model/Type.hpp>
-
 #include <Bull/Graphics/Model/Mesh.hpp>
+#include <Bull/Graphics/Model/Node.hpp>
 
 class Cube : public Bull::Node
 {
