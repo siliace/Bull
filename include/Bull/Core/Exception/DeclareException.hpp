8ad093740639da6cd308d71c568a98953238387e
Siliace <benjaminbernard1995@gmail.com>
2018-08-04 21:57:02 +0200

    [Core/DeclareException] Add a macro to declare exception with a parent class other than Bull::Exception

--- include/Bull/Core/Exception/DeclareException.hpp
+++ include/Bull/Core/Exception/DeclareException.hpp
@@ -3,14 +3,16 @@
 
 #include <Bull/Core/Exception/Exception.hpp>
 
-#define DeclareException(ExceptionName)                                                               \
-struct ExceptionName : public Exception                                                               \
+#define DeclareExceptionFrom(ExceptionName, ParentException)                                          \
+struct ExceptionName : public ParentException                                                         \
 {                                                                                                     \
     ExceptionName(const String& file, Uint64 line, const String& source, const String& description) : \
-        Exception(file, line, #ExceptionName, source, description)                                    \
+        ParentException(file, line, #ExceptionName, source, description)                              \
     {                                                                                                 \
                                                                                                       \
     }                                                                                                 \
 };
+
+#define DeclareException(ExceptionName) DeclareExceptionFrom(ExceptionName, Bull::Exception)
 
 #endif // BULL_CORE_EXCEPTION_DECLAREEXCEPTION_HPP
