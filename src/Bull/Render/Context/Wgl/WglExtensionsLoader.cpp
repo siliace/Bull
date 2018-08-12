470abf287cea2ee31735b956c9f47a14ab460e84
Siliace <benjber69@gmail.com>
2018-08-12 22:35:32 +0200

    Fix build

--- src/Bull/Render/Context/Wgl/WglExtensionsLoader.cpp
+++ src/Bull/Render/Context/Wgl/WglExtensionsLoader.cpp
@@ -1,6 +1,4 @@
-#include <Bull/Core/Exception/Expect.hpp>
 #include <Bull/Core/Exception/InternalError.hpp>
-#include <Bull/Core/Exception/Throw.hpp>
 #include <Bull/Core/Support/Win32/Windows.hpp>
 
 #include <Bull/Render/Context/GlContext.hpp>
