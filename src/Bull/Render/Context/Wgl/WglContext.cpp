166e6368388f14c81b58d1151cd24fd9096fea29
Siliace <benjber69@gmail.com>
2018-08-13 17:56:28 +0200

    [Render/WglContext] Enable PBuffer if available

--- src/Bull/Render/Context/Wgl/WglContext.cpp
+++ src/Bull/Render/Context/Wgl/WglContext.cpp
@@ -44,6 +44,7 @@ namespace Bull
             loader->require(wglCreateContext);
             loader->require(wglPixelFormat);
             loader->require(wglSwapControl);
+            loader->require(wglPbuffer);
         }
 
         int WglContext::getBestPixelFormat(HDC device, Uint8 bitsPerPixel, const ContextSettings& settings, bool usePbuffer)
